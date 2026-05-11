eb6d0afb079b6310c28eb92c2ac1d7b4d792c3f4
Rajkumar <mandarajkumar2001>
2026-05-11 16:09:21 +0530

    converstion

--- 02-convertion.sh
+++ 02-convertion.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-echo "raj : hi ravi"
-echo "ravi : hi raju"
-echo "raj : how are you"
-echo "ravi : fine! you?"
-echo "raj  : ok bye"
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hey $PERSON2, How are you?"
+echo "$PERSON2:: Hello $PERSON1, I am fine. How are you doing"
+echo "$PERSON1:: I am fine too. What's up?"
+echo "$PERSON2:: Nothing, just going to Mars now, will you come?"
+echo "$PERSON1:: Sorry, you carry on! I will come once you come back"
